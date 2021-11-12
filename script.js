8890bd2927ae7b6a6808f9b7be2042c5a0b00dd2
Luiz Cipriano <77516067+luizcipriano@users.noreply.github.com>
2021-11-12 20:25:06 -0300

    -- First page animated

---
+++ script.js
@@ -0,0 +1,19 @@
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.front-page',
+        start: '0%',
+        end: '100%',
+        scrub: true,
+        pin: true
+    }
+})
+
+tl.fromTo(".front-page",
+{clipPath: "circle(5%)"},
+{clipPath: "circle(75%)", duration: 3 }
+);
+tl.fromTo('.music-note', {scale: 0.5}, {scale: 0, opacity: 0, duration: 1 ,}, "-=3"
+);
+
+tl.fromTo('.title', {opacity: 0 }, {opacity: 1, duration: 1 });
+tl.fromTo('.sub-title', {opacity: 0 }, {opacity: 1, duration: 1 });
